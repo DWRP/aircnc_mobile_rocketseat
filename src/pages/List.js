c5384b6d60432118e47202fe752441d70d43dc2b
Douglas Walas <douglas.skd@gmail.com>
2020-02-02 15:55:41 -0300

    finsh app

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,6 +1,8 @@
 import React,{ useState,useEffect } from 'react';
-import { View, ScrollView, Text, AsyncStorage, Image, TouchableOpacity } from 'react-native';
+import { View, ScrollView, Text, AsyncStorage, Image, TouchableOpacity, Alert } from 'react-native';
+import socketio from 'socket.io-client';
 
+import api from '../services/api';
 import SpotList from '../components/SpotList';
 import styles from '../assets/styles/GlobalStyle';
 
@@ -8,7 +10,22 @@ import logo from '../assets/images/aircnc_logo.png';
 
 export default function List({navigation}) {
     const [techs, setTechs] = useState([]);
-    
+    useEffect(()=>{
+        AsyncStorage.getItem('user').then(user_id=>{
+            const socket = socketio(`${api.defaults.baseURL}`, {
+                query: { user_id }
+            });
+            socket.on('booking_response', booking => {
+                Alert.alert(`Reserva ${booking.approved?'Aprovada':'Rejeitada'}`,
+                    `Sua reserva em ${booking.spot.company} em ${booking.date} foi ${booking.approved?'APROVADA':'REJEITADA'}`,
+                    [
+                        {text: 'Continuar!'},
+                    ],
+                    {cancelable: false});
+            });
+        });
+    },[]);
+
     useEffect(()=>{
         AsyncStorage.getItem('techs').then( techsItems =>{
             const techsArray = techsItems.split(',').map(tech=>tech.trim());
